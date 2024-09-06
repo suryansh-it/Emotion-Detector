c7a4faaf9df54daa0e76eabf35b5026a214cc2b5
Suryansh Sharma <suryansharma09@gmail.com>
2024-09-06 16:06:00 +0530

    base flask structure done

--- app.py
+++ app.py
@@ -1,2 +1,19 @@
 from flask import Flask , render_template
 
+
+def create_app():
+    app = Flask(__name__)
+
+app = create_app()
+
+
+@app.route('/')
+def home() :
+    render_template('index.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
